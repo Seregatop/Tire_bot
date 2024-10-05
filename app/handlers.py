4cb3328b50c73ba0ecc5e388ab37ac4bd6a0ee2c
Sergulyok <STIFT1997@mail.ru>
2024-10-05 18:12:25 +0300

    Кнопка Cancel

--- app/handlers.py
+++ app/handlers.py
@@ -9,7 +9,6 @@ from app.keyboards import keyboard_inline_new, keyboard_inline_post
 
 from app.states import Reg
 from aiogram.fsm.context import FSMContext
-
 from datetime import datetime
 
 from app.sending_to_sheets import write_row
@@ -23,6 +22,29 @@ async def cmd_start(message: Message, state: FSMContext):
     await message.answer('Касса-бот, /car для начала', reply_markup=ReplyKeyboardRemove())
 
 
+@user.callback_query(F.data == 'cancel')
+async def call_cancel(call: CallbackQuery, state: FSMContext):
+    await call.message.delete()
+    await state.clear()
+
+
+# доработать
+@user.callback_query(F.data == 'back')
+async def call_back(call: CallbackQuery, state: FSMContext):
+    match await state.get_state():
+        case Reg.wait_for_diameter:
+            return
+        case Reg.wait_for_service:
+            await state.set_state(Reg.wait_for_diameter)
+            await sell_call_start(call, state)
+        case Reg.wait_for_additional_service:
+            await state.set_state(Reg.wait_for_service)
+            await diameter_chosen(call, state)
+        case Reg.wait_for_payment_type:
+            await state.set_state(Reg.wait_for_additional_service)
+            await service_chosen(call, state)
+
+
 @user.message(Command('car'))
 async def sell_start(message: Message, state: FSMContext):
     await message.delete()
@@ -32,8 +54,10 @@ async def sell_start(message: Message, state: FSMContext):
 
 @user.callback_query(F.data == 'car')
 async def sell_call_start(call: CallbackQuery, state: FSMContext):
+    await call.message.delete_reply_markup()
     await call.message.answer('1. Выберите диаметр колеса', reply_markup=await available_kb(DiameterDB))
     await state.set_state(Reg.wait_for_diameter)
+    await state.update_data(DB=DiameterDB)
 
 
 @user.callback_query(Reg.wait_for_diameter)
@@ -44,6 +68,7 @@ async def diameter_chosen(call: CallbackQuery, state: FSMContext):
     await state.update_data(chosen_diameter=call.data)
     await state.set_state(Reg.wait_for_service)
     await call.message.edit_text(text='2. Выберите услугу', reply_markup=await available_kb(ServiceDB))
+    await state.update_data(message_id=call.message.message_id)
 
 
 @user.callback_query(Reg.wait_for_service)
@@ -89,37 +114,38 @@ async def discount_chosen(call: CallbackQuery, state: FSMContext):
 
 
 @user.message(Reg.wait_for_price)
-async def price_chosen(msg: Message, state: FSMContext):
+async def price_chosen(message: Message, state: FSMContext):
     try:
-        int(msg.text.lower())
+        int(message.text.lower())
     except ValueError:
-        await msg.answer(text='Цифру без всего')
+        await message.answer(text='Цифру без всего')
         return
-    await state.update_data(chosen_price=msg.text)
+    await state.update_data(chosen_price=message.text)
     await state.set_state(Reg.wait_for_send)
     user_data = await state.get_data()
-    await msg.answer(text=
-                     f'Диаметр: {user_data["chosen_diameter"]}\n'
-                     f'Услуга: {user_data["chosen_service"]}\n'
-                     f'Допулсуга: {user_data["chosen_additional_service"]}\n'
-                     f'Вид оплаты: {user_data["chosen_payment_type"]}\n'
-                     f'{user_data["chosen_discount"]}% скидка\n'
-                     f'Сумма: {user_data["chosen_price"]} руб\\.\n', reply_markup=keyboard_inline_post)
+    await message.bot.delete_message(chat_id=message.chat.id, message_id=int(user_data['message_id']))
+    await message.delete()
+    await message.answer(text=
+                         f'Диаметр: {user_data["chosen_diameter"]}\n'
+                         f'Услуга: {user_data["chosen_service"]}\n'
+                         f'Допулсуга: {user_data["chosen_additional_service"]}\n'
+                         f'Вид оплаты: {user_data["chosen_payment_type"]}\n'
+                         f'{user_data["chosen_discount"]}% скидка\n'
+                         f'Сумма: {user_data["chosen_price"]} руб.\n', reply_markup=keyboard_inline_post)
 
 
 @user.callback_query(Reg.wait_for_send)
 async def send_chosen(call: CallbackQuery, state: FSMContext):
     user_data = await state.get_data()
-    # дата
-    #bot.answer_callback_query()
     now = datetime.now()
     new_years = datetime(day=30, month=12, year=1899)
     countdown = now - new_years
     await write_row(
-        [int(countdown.days), int(countdown.days), str(call.message.date.time()), str(call.message.chat.full_name),
-         call.from_user.id, call.from_user.username, user_data["chosen_diameter"],
-         str(user_data["chosen_service"]), user_data["chosen_additional_service"],
-         "", user_data["chosen_payment_type"], int(user_data["chosen_discount"]), int(user_data["chosen_price"])])
+        [int(countdown.days), int(countdown.days), int(call.message.date.month), str(call.message.date.time()),
+         str(call.message.chat.full_name), call.from_user.id, call.from_user.username,
+         user_data["chosen_diameter"], str(user_data["chosen_service"]), user_data["chosen_additional_service"],
+         "", user_data["chosen_payment_type"], round(float(user_data["chosen_discount"]) / 100, 2),
+         int(user_data["chosen_price"])])
     await call.answer(text="Отправлено")
     await call.message.edit_reply_markup(reply_markup=keyboard_inline_new)
     print(call.message.chat.username, user_data["chosen_price"])
