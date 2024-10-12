6b31e19ffd240fe7d730dd4915390826afca7fbe
Sergulyok <STIFT1997@mail.ru>
2024-10-12 20:27:47 +0300

    Добавил состояние Расходы

--- app/states.py
+++ app/states.py
@@ -1,7 +1,7 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class Reg(StatesGroup):
+class Sale(StatesGroup):
     wait_for_diameter = State()
     wait_for_service = State()
     wait_for_additional_service = State()
@@ -9,3 +9,10 @@ class Reg(StatesGroup):
     wait_for_discount = State()
     wait_for_price = State()
     wait_for_send = State()
+
+
+class Pay(StatesGroup):
+    wait_for_category = State()
+    wait_for_payer = State()
+    wait_for_object = State()
+    wait_for_price = State()
